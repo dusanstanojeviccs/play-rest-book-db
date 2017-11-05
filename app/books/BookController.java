4e59d2d625a7f62aa939e55e371c4eacf4f91918
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2017-11-05 13:48:28 -0800

    init

---
+++ app/books/BookController.java
@@ -0,0 +1,54 @@
+package books;
+
+import com.google.inject.Inject;
+import play.db.Database;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BookController extends Controller {
+
+    @Inject public BookRepository bookRepository;
+    @Inject Database database;
+
+    public Result findAll() {
+        return database.withConnection(conn -> {
+            return ok(Json.toJson(bookRepository.findAll())).as("application/json");
+        });
+    }
+
+    public Result findById(int id) {
+        return database.withConnection(conn -> {
+            return ok(Json.toJson(bookRepository.findById(id))).as("application/json");
+        });
+    }
+
+    public Result create() {
+        return database.withConnection(conn -> {
+            Book bookRequest = Json.fromJson(request().body().asJson(), Book.class);
+
+            bookRepository.add(bookRequest);
+
+            return ok(Json.toJson(bookRequest)).as("application/json");
+        });
+    }
+
+    public Result update(int id) {
+        return database.withConnection(conn -> {
+            Book bookRequest = Json.fromJson(request().body().asJson(), Book.class);
+
+            bookRequest.setId(id);
+
+            bookRepository.update(bookRequest);
+
+            return ok(Json.toJson(bookRequest)).as("application/json");
+        });
+    }
+
+    public Result delete(int id) {
+        return database.withConnection(conn -> {
+            bookRepository.delete(id);
+            return ok("{}").as("application/json");
+        });
+    }
+}
---
+++ app/books/BookRepository.java
@@ -0,0 +1,96 @@
+package books;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import java.sql.*;
+import java.util.*;
+
+import play.db.*;
+
+@Singleton
+public class BookRepository {
+    @Inject Database database;
+
+    public List<Book> findAll() throws SQLException {
+        PreparedStatement preparedStatement = database.getConnection().prepareStatement("select * from books");
+
+        List<Book> books = new ArrayList<>();
+
+        preparedStatement.execute();
+
+        ResultSet rs = preparedStatement.getResultSet();
+
+        while (rs.next()) {
+            Book book = new Book();
+
+            book.setId(rs.getInt("id"));
+            book.setTitle(rs.getString("title"));
+            book.setDescription(rs.getString("description"));
+
+            books.add(book);
+        }
+
+        return books;
+    }
+
+    public Optional<Book> findById(int id) throws SQLException {
+        PreparedStatement preparedStatement = database.getConnection().prepareStatement("select * from books where id = ?");
+
+        preparedStatement.setInt(1, id);
+
+        preparedStatement.execute();
+
+        ResultSet rs = preparedStatement.getResultSet();
+
+        if (rs.next()) {
+            Book book = new Book();
+
+            book.setId(rs.getInt("id"));
+            book.setTitle(rs.getString("title"));
+            book.setDescription(rs.getString("description"));
+
+
+            return Optional.of(book);
+        }
+
+        return Optional.empty();
+    }
+
+    public void delete(int id) throws SQLException {
+        PreparedStatement preparedStatement = database.getConnection().prepareStatement("delete from books where id = ?");
+
+        preparedStatement.setInt(1, id);
+
+        preparedStatement.execute();
+    }
+
+    public void add(Book book) throws SQLException {
+        // Book has no id because we are adding a new one to the database
+        PreparedStatement preparedStatement = database.getConnection().prepareStatement("insert into books (id, title, description) values (null, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, book.getTitle());
+        preparedStatement.setString(2, book.getDescription());
+
+        preparedStatement.execute();
+
+        // after inserting we want to get the generated id
+        ResultSet rs = preparedStatement.getGeneratedKeys();
+
+        if (rs.next()) {
+            book.setId(rs.getInt(1));
+        }
+
+        preparedStatement.executeUpdate();
+    }
+
+    public void update(Book book) throws SQLException {
+        PreparedStatement preparedStatement = database.getConnection().prepareStatement("update books set title = ?, description = ? where id = ?");
+
+        preparedStatement.setString(1, book.getTitle());
+        preparedStatement.setString(2, book.getDescription());
+        preparedStatement.setInt(3, book.getId());
+
+        preparedStatement.executeUpdate();
+    }
+
+}
